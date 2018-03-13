2f4504e18813b4977fb2b53003fd93b85a0d6da3
piros gyb <pirosgyb@edu.bme.hu>
2018-03-13 12:21:06 +0100

    0.2 done
    A megfelelp scenekre valt, de vissza meg nem lehet valtani.

---
+++ Assets/MyStaff/Scripts/DesignRoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+[RequireComponent(typeof(Collider))]
+public class DesignRoom : MonoBehaviour {
+	private Vector3 startingPosition;
+	private Renderer renderer;
+
+
+	public Material inactiveMaterial;
+	public Material gazedAtMaterial;
+
+	void Start() {
+		startingPosition = transform.localPosition;
+		renderer = GetComponent<Renderer>();
+		SetGazedAt(false);
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		if (inactiveMaterial != null && gazedAtMaterial != null) {
+			renderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial ;
+			return;
+		}
+	}
+
+	public void SetClicked(){
+		StartCoroutine (LoadAsyncScene ());
+		SetGazedAt(false);
+		return;
+	}
+	IEnumerator LoadAsyncScene(){
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync ("DesignRoom",LoadSceneMode.Additive);
+
+		while (!asyncLoad.isDone) {
+			yield return null;
+		}
+	}
+}
+
---
+++ Assets/MyStaff/Scripts/MakeRoom.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+[RequireComponent(typeof(Collider))]
+public class MakeRoom : MonoBehaviour {
+	private Vector3 startingPosition;
+	private Renderer renderer;
+
+
+	public Material inactiveMaterial;
+	public Material gazedAtMaterial;
+
+
+	void Start() {
+		startingPosition = transform.localPosition;
+		renderer = GetComponent<Renderer>();
+		SetGazedAt(false);
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		if (inactiveMaterial != null && gazedAtMaterial != null) {
+			renderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial ;
+			return;
+		}
+	}
+
+	public void SetClicked(){
+		StartCoroutine (LoadAsyncScene ());
+		SetGazedAt(false);
+		return;
+	}
+
+	IEnumerator LoadAsyncScene(){
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync ("MakeRoom",LoadSceneMode.Additive);
+
+		while(!asyncLoad.isDone){
+			yield return null;
+		}
+
+	}
+
+
+}
+
