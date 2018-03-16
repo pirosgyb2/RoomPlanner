7a5f022aea72b73f2f2e6dab8cd2f13736019da3
piros gyb <pirosgyb@edu.bme.hu>
2018-03-16 10:46:17 +0100

    1.2.3.1 done
    Menubol lehet falat hozzaadni.

--- Assets/MyStaff/Scripts/MakeRoomMenu.cs
+++ Assets/MyStaff/Scripts/MakeRoomMenu.cs
@@ -6,17 +6,38 @@ using DaydreamElements.ClickMenu;
 
 public class MakeRoomMenu : MonoBehaviour {
 
-	void Start(){
-		
+	public GameObject wall;
+
+	void Start(){		
 		gameObject.GetComponent<ClickMenuRoot> ().OnItemSelected += Switcher;
 	}
 
 	void Switcher(ClickMenuItem item){
 		if (item != null) {
 			switch (item.id) {
-			case 0:
+			case 0: //Home
 				StartCoroutine (LoadAsyncScene ());
 				break;
+			case 1: //Add
+				Instantiate (wall,new Vector3(0,0.5f,1),Quaternion.identity);
+				break;
+			case 2: //File
+				break;
+			case 3: //View
+				break;
+			case 4: //Delete
+				break;
+			case 5: //Load
+				break;
+			case 6: //Save
+				break;
+			case 7: //TopView
+				break;
+			case 8: //SideView
+				break;
+			case 9: //PerspView
+				break;
+
 			default:
 				break;			
 			}
